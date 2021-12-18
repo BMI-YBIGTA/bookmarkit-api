0dc5419f91a6c8c04b7a698cbc43604f9bbf0945
jyunkim <jihyun.kim@bucketplace.net>
2021-12-18 21:46:04 +0900

    Initial commit

---
+++ src/main/java/com/bmi/bookmarkitapi/BookmarkitApiApplication.java
@@ -0,0 +1,13 @@
+package com.bmi.bookmarkitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmi/bookmarkitapi/BookmarkitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmi.bookmarkitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
