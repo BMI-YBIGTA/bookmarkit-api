9e3784440b4e44b6f7409d5b76e1aa5b7159cdd4
Jaeyeon Park <donionb@yonsei.ac.kr>
2021-12-20 20:18:14 +0900

    fix: fix securityConfiguration

--- src/main/java/com/bmi/bookmarkitapi/common/config/WebSecurityConfiguration.java
+++ src/main/java/com/bmi/bookmarkitapi/common/config/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**","/memberbookmark/**");
+        web.ignoring().antMatchers("/v3/api-docs/**",
+                "/swagger-ui.html", "/swagger-ui/**","/memberbookmark/**","/**");
     }
 }
