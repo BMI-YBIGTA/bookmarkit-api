99800f94e0bfc0e389392aa70ad2238cd4c236ae
jyunkim <jihyun.kim@bucketplace.net>
2021-12-21 23:27:40 +0900

    refactor: change parent exception class

--- src/main/java/com/bmi/bookmarkitapi/common/exception/NotFoundException.java
+++ src/main/java/com/bmi/bookmarkitapi/common/exception/NotFoundException.java
@@ -1,6 +1,7 @@
 package com.bmi.bookmarkitapi.common.exception;
 
-public abstract class NotFoundException extends IllegalArgumentException {
+public abstract class NotFoundException extends CommonException {
+
     public NotFoundException(String message) {
         super(message);
     }
