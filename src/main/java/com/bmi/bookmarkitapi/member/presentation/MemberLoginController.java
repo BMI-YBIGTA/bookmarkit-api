5d4861df44ca997c88cf1440b3c92ed72e86d7f7
jyunkim <jihyun.kim@bucketplace.net>
2021-12-22 00:22:37 +0900

    fix: add RequestBody annotation

--- src/main/java/com/bmi/bookmarkitapi/member/presentation/MemberLoginController.java
+++ src/main/java/com/bmi/bookmarkitapi/member/presentation/MemberLoginController.java
@@ -5,6 +5,7 @@ import com.bmi.bookmarkitapi.member.application.model.MemberDto;
 import com.bmi.bookmarkitapi.member.application.service.MemberLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class MemberLoginController {
     private final MemberLoginService memberLoginService;
 
     @PostMapping("/sign-in")
-    public Response.Item<String> signIn(MemberDto.Request.Login memberDto) {
+    public Response.Item<String> signIn(@RequestBody MemberDto.Request.Login memberDto) {
         return new Response.Item<>(memberLoginService.login(memberDto));
     }
 }
