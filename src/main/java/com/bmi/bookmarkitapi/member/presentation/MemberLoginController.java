32d8fa5c8a34d34ebae01a94b61a2562b1dd0260
jyunkim <jihyun.kim@bucketplace.net>
2021-12-21 22:51:34 +0900

    feat: add presentation layer controllers

---
+++ src/main/java/com/bmi/bookmarkitapi/member/presentation/MemberLoginController.java
@@ -0,0 +1,22 @@
+package com.bmi.bookmarkitapi.member.presentation;
+
+import com.bmi.bookmarkitapi.common.dto.Response;
+import com.bmi.bookmarkitapi.member.application.model.MemberDto;
+import com.bmi.bookmarkitapi.member.application.service.MemberLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MemberLoginController {
+
+    private final MemberLoginService memberLoginService;
+
+    @PostMapping("/sign-in")
+    public Response.Item<String> signIn(MemberDto.Request.Login memberDto) {
+        return new Response.Item<>(memberLoginService.login(memberDto));
+    }
+}
