17655fafb6f5e9d470b233756e5716d907f8b4d7
jyunkim <jihyun.kim@bucketplace.net>
2021-12-23 01:12:05 +0900

    feat: add category repository

---
+++ src/main/java/com/bmi/bookmarkitapi/category/domain/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.bmi.bookmarkitapi.category.domain.repository;
+
+import com.bmi.bookmarkitapi.category.domain.model.Category;
+import com.bmi.bookmarkitapi.common.BaseRepository;
+
+public interface CategoryRepository extends BaseRepository<Category> {
+}
