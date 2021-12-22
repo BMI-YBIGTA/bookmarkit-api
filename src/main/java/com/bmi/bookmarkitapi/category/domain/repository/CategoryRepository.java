c81bb57b74428a71e01bf5344bbe69f4f5944421
jyunkim <jihyun.kim@bucketplace.net>
2021-12-23 01:27:01 +0900

    feat: add read query method

--- src/main/java/com/bmi/bookmarkitapi/category/domain/repository/CategoryRepository.java
+++ src/main/java/com/bmi/bookmarkitapi/category/domain/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.bmi.bookmarkitapi.category.domain.repository;
 import com.bmi.bookmarkitapi.category.domain.model.Category;
 import com.bmi.bookmarkitapi.common.BaseRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends BaseRepository<Category> {
+
+    Optional<Category> findByMainCategoryAndSubCategory(String mainCategory, String subCategory);
 }
