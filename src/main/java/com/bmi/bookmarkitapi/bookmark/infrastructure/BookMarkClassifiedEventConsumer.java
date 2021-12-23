74ce86e51a2cb54a11e590f0309fd20526ed9984
jyunkim <jihyun.kim@bucketplace.net>
2021-12-24 01:15:22 +0900

    refactor: replace print with log

--- src/main/java/com/bmi/bookmarkitapi/bookmark/infrastructure/BookMarkClassifiedEventConsumer.java
+++ src/main/java/com/bmi/bookmarkitapi/bookmark/infrastructure/BookMarkClassifiedEventConsumer.java
@@ -3,11 +3,13 @@ package com.bmi.bookmarkitapi.bookmark.infrastructure;
 import com.bmi.bookmarkitapi.bookmark.application.BookMarkCategorySettingService;
 import com.bmi.bookmarkitapi.bookmark.application.BookMarkStatusModificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import java.util.StringTokenizer;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookMarkClassifiedEventConsumer {
@@ -17,12 +19,15 @@ public class BookMarkClassifiedEventConsumer {
     @RabbitListener(queues = "q.classification.classified")
     public void listen(String message) {
         StringTokenizer stringTokenizer = new StringTokenizer(message, "|||");
+
         Long bookMarkId = Long.parseLong(stringTokenizer.nextToken());
         String mainCategory = stringTokenizer.nextToken();
         String subCategory = stringTokenizer.nextToken();
-        System.out.println("bookmarkId: " + bookMarkId);
-        System.out.println("mainCategory: " + mainCategory);
-        System.out.println("subCategory: " + subCategory);
+
+        log.info("bookmarkId: " + bookMarkId);
+        log.info("mainCategory: " + mainCategory);
+        log.info("subCategory: " + subCategory);
+
         bookMarkStatusModificationService.complete(bookMarkId);
         bookMarkCategorySettingService.set(bookMarkId, mainCategory, subCategory);
     }
