98f5154d5751567e42e0be0ecb77f19edb883b83
jyunkim <jyunkim13@gmail.com>
2022-03-17 14:36:18 +0900

    style: dto 이름 수정

--- src/main/java/com/bmi/bookmarkitapi/member/application/model/request/MemberModifyInfoRequest.java
+++ src/main/java/com/bmi/bookmarkitapi/member/application/model/request/MemberModifyInfoRequest.java
@@ -1,21 +1,16 @@
 package com.bmi.bookmarkitapi.member.application.model.request;
 
-import com.bmi.bookmarkitapi.member.domain.model.Member;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
-public class MemberRegisterRequestDto {
+public class MemberModifyInfoRequest {
     @Email
     private String email;
     @NotBlank
     private String name;
     @NotBlank
     private String password;
-
-    public Member toEntity() {
-        return new Member(email, name, password);
-    }
 }
--- src/main/java/com/bmi/bookmarkitapi/member/presentation/MemberController.java
+++ src/main/java/com/bmi/bookmarkitapi/member/presentation/MemberController.java
@@ -1,11 +1,11 @@
 package com.bmi.bookmarkitapi.member.presentation;
 
 import com.bmi.bookmarkitapi.common.dto.Response;
-import com.bmi.bookmarkitapi.member.application.model.request.MemberLoginRequestDto;
-import com.bmi.bookmarkitapi.member.application.model.request.MemberModifyInfoRequestDto;
-import com.bmi.bookmarkitapi.member.application.model.request.MemberRegisterRequestDto;
-import com.bmi.bookmarkitapi.member.application.model.response.MemberLoginResponseDto;
-import com.bmi.bookmarkitapi.member.application.model.response.MemberResponseDto;
+import com.bmi.bookmarkitapi.member.application.model.request.MemberLoginRequest;
+import com.bmi.bookmarkitapi.member.application.model.request.MemberModifyInfoRequest;
+import com.bmi.bookmarkitapi.member.application.model.request.MemberRegisterRequest;
+import com.bmi.bookmarkitapi.member.application.model.response.MemberLoginResponse;
+import com.bmi.bookmarkitapi.member.application.model.response.MemberResponse;
 import com.bmi.bookmarkitapi.member.application.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,25 +21,25 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
-    public Response.Item<MemberResponseDto> getInfo(@PathVariable Long id) {
+    public Response.Item<MemberResponse> getInfo(@PathVariable Long id) {
         return new Response.Item<>(memberService.getInfo(id));
     }
 
     @PutMapping("/{id}")
-    public Response.Item<MemberResponseDto> modifyInfo(
+    public Response.Item<MemberResponse> modifyInfo(
             @PathVariable Long id,
-            @Valid @RequestBody MemberModifyInfoRequestDto request
+            @Valid @RequestBody MemberModifyInfoRequest request
     ) {
         return new Response.Item<>(memberService.modifyInfo(id, request));
     }
 
     @PostMapping("/sign-up")
-    public Response.Item<MemberResponseDto> signUp(@Valid @RequestBody MemberRegisterRequestDto request) {
+    public Response.Item<MemberResponse> signUp(@Valid @RequestBody MemberRegisterRequest request) {
         return new Response.Item<>(memberService.register(request));
     }
 
     @PostMapping("/sign-in")
-    public Response.Item<MemberLoginResponseDto> signIn(@Valid @RequestBody MemberLoginRequestDto request) {
+    public Response.Item<MemberLoginResponse> signIn(@Valid @RequestBody MemberLoginRequest request) {
         return new Response.Item<>(memberService.login(request));
     }
 }
