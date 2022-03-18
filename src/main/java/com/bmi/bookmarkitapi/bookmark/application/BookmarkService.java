1006932f09181b39c236134650a4829c3e666a7b
jyunkim <jyunkim13@gmail.com>
2022-03-18 23:18:56 +0900

    style: remove unused classes

--- src/main/java/com/bmi/bookmarkitapi/bookmark/application/BookmarkService.java
+++ src/main/java/com/bmi/bookmarkitapi/bookmark/application/BookmarkService.java
@@ -1,19 +1,9 @@
 package com.bmi.bookmarkitapi.bookmark.application;
 
 import com.bmi.bookmarkitapi.bookmark.application.model.request.BookmarkRegisterRequest;
-import com.bmi.bookmarkitapi.bookmark.application.model.response.BookmarkResponse;
 import com.bmi.bookmarkitapi.bookmark.domain.model.Bookmark;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
 
 public interface BookmarkService {
-
-    List<BookmarkResponse> findAll();
-
-    BookmarkResponse findById(Long id);
-
-    Page<BookmarkResponse> findByPage(int page, int size);
 
     Bookmark register(BookmarkRegisterRequest request);
 }
