5b8d5a1dafe698ddb2a3c9f1e82d66ea89bd1cac
jyunkim <jyunkim13@gmail.com>
2022-04-06 23:44:09 +0900

    fix: fix platform issue

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,6 @@
 
 ./gradlew build
 
-docker build -t jyunkim/bookmarkit-api:dev --build-arg APP_PROFILE=dev .
+docker build -t jyunkim/bookmarkit-api:dev --build-arg APP_PROFILE=dev --platform linux/amd64 .
 cat ./secret.txt | docker login -u jyunkim --password-stdin
 docker push jyunkim/bookmarkit-api:dev
